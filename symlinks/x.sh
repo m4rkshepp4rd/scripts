e5d0b0cb341061a6adccd85c2dccdf822f26cdb2
Mark Sheppard <m4rkshepp4rd@yandex.ru>
2025-10-26 17:00:07 +0500

    symlinks inside project folder

--- symlinks/x.sh
+++ symlinks/x.sh
@@ -6,7 +6,17 @@ if [[ -z $MS_SCR ]]; then
 fi
 
 rm -rf $HOME/.x
+ln -s $MS_SCR $HOME/.x
 
-if [[ -z $1 ]]; then 
-    ln -s $MS_SCR $HOME/.x
+if [[ " $* " == *" -l "* ]]; then
+    if [[ -z $MS_DOCS ]]; then
+        echo "($(basename $0))" "Env var MS_DOCS not defined"
+        exit 1
+    fi
+    rm "$MS_SCR/_configs"
+    ln -s "$MS_DOCS/configs" "$MS_SCR/_configs"
+    rm "$MS_SCR/_cfg-backups"
+    ln -s "$MS_DOCS/cfg-backups" "$MS_SCR/_cfg-backups"
+    rm "$MS_SCR/todo.md"
+    ln -s "$MS_DOCS/todo.md" "$MS_SCR/todo.md"
 fi
