58d9b2bc461873ede384d1e7af52a321a23a36f8
Mark Sheppard <m4rkshepp4rd@yandex.ru>
2025-11-12 23:16:59 +0500

    chmod700 refactor

--- chmod700.sh
+++ chmod700.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-if [[ -z $1 ]]; then
-    echo "($(basename $0))" "enter path"
-    exit 1
-fi
+chmod_path="$1"
 
-for path in $(find $1 -name '*'); do
-    if [[ -f ${path} ]]; then
-        chmod 600 ${path}
-        echo "($(basename $0))" "chmod 600 ${path}"
+set -e
+x-utils-check var $0 chmod_path
+set +e
+
+for path in $(find $chmod_path -name '*'); do
+    if [[ -f $path ]]; then
+        chmod 600 $path
+        echo "$(basename $0): chmod 600 $path"
     fi
-    if [[ -d ${path} ]]; then
-        chmod 700 ${path}
-        echo "($(basename $0))" "chmod 700 ${path}"
+    if [[ -d $path ]]; then
+        chmod 700 $path
+        echo "$(basename $0): chmod 700 $path"
     fi
 done
