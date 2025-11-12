900e014122c48f82cfd09b3892beee3ca5b9a09a
Mark Sheppard <m4rkshepp4rd@yandex.ru>
2025-11-13 00:32:01 +0500

    removed etc

--- etc/foreach.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-for path in $(find Documents/_docs/scripts -name '*.sh'); do
-    sed -e 's;echo "($(basename $0))";echo "($(basename $0))";' $path > $path.tmp && mv $path.tmp $path
-done    
