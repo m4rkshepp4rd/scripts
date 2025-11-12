73e744d4b88455ac14d2930020ef5c8216f97bdd
Mark Sheppard <m4rkshepp4rd@yandex.ru>
2025-11-12 23:11:51 +0500

    cfg-backup refactor

--- cfg-backups.sh
+++ cfg-backups.sh
@@ -1,76 +1,50 @@
 #!/usr/bin/env bash
 
-if [[ -z $MS_CFG_BACKUPS ]]; then
-    echo "($(basename $0))" "Env var MS_CFG_BACKUPS not defined"
-    exit 1
-fi
-
-if [[ ! -d "$MS_CFG_BACKUPS" ]]; then
-    echo "($(basename $0))" "Backups folder not found"
-    exit 1
-fi
+set -e
+x-utils-check var $0 MS_CFG_BACKUPS
+x-utils-check dir $0 $MS_CFG_BACKUPS
+set +e
 
 config_fld="$MS_CFG_BACKUPS/$(hostname)"
 
-rm -rf "$config_fld/bash"
-mkdir -p "$config_fld/bash"
+rmkdir() {
+    rm -rf "$config_fld/$1"
+    mkdir -p "$config_fld/$1"
+}
+
+backup_cfg() {
+    for fld in "$@"; do
+        rmkdir $fld
+        cp -r $HOME/.config/$fld/* "$config_fld/$fld"
+    done
+}
+
+rmkdir env
+cp -f $HOME/.sharenv "${config_fld}/env/.sharenv"
+cp -f $HOME/.config/.myhardware.yml "${config_fld}/env/.myhardware.yml"
+
+rmkdir bash
 cp -f $HOME/.bashrc "${config_fld}/bash/.bashrc"
 
-rm -rf "$config_fld/zsh"
-mkdir -p "$config_fld/zsh"
+rmkdir zsh
 cp -f $HOME/.zshrc "${config_fld}/zsh/.zshrc"
 cp -f $HOME/.zshenv "${config_fld}/zsh/.zshenv"
 cp -f $HOME/.p10k.zsh "${config_fld}/zsh/.p10k.zsh"
 
-rm -rf "$config_fld/env"
-mkdir -p "$config_fld/env"
-cp -f $HOME/.sharenv "${config_fld}/env/.sharenv"
-cp -f $HOME/.config/.myhardware.yml "${config_fld}/env/.myhardware.yml"
-
-rm -rf "$config_fld/flameshot"
-mkdir -p "$config_fld/flameshot"
-cp -f $HOME/.config/flameshot/flameshot.ini "${config_fld}/flameshot/flameshot.ini"
-
-rm -rf "$config_fld/copyq"
-mkdir -p "$config_fld/copyq"
-cp -f $HOME/.config/copyq/copyq.conf "${config_fld}/copyq/copyq.conf"
-
-rm -rf "$config_fld/tmux"
-mkdir -p "$config_fld/tmux"
-cp -f $HOME/.config/tmux/tmux.conf "${config_fld}/tmux/tmux.conf"
-
-rm -rf "$config_fld/onlyoffice"
-mkdir -p "$config_fld/onlyoffice"
-cp -f $HOME/.config/onlyoffice/DesktopEditors.conf "${config_fld}/onlyoffice/DesktopEditors.conf"
-
-rm -rf "$config_fld/pop-shell"
-mkdir -p "$config_fld/pop-shell"
-cp -f $HOME/.config/pop-shell/config.json "${config_fld}/pop-shell/config.json"
-
-rm -rf "$config_fld/yandex-disk"
-mkdir -p "$config_fld/yandex-disk"
-cp -f $HOME/.config/yandex-disk/config.cfg "${config_fld}/yandex-disk/config.cfg"
-
-rm -rf "$config_fld/telegram"
-mkdir -p "$config_fld/telegram"
+rmkdir telegram
 cp -f $HOME/.local/share/TelegramDesktop/tdata/shortcuts-custom.json "${config_fld}/telegram/shortcuts-custom.json"
 
-rm -rf "$config_fld/vscode"
-mkdir -p "$config_fld/vscode"
+rmkdir vscode
 code-insiders --list-extensions > "${config_fld}/vscode/extensions.txt"
 cp -f $HOME/.config/Code\ -\ Insiders/User/settings.json "${config_fld}/vscode/settings.json"
 
-rm -rf "$config_fld/cron"
-mkdir -p "$config_fld/cron"
+rmkdir cron
 crontab -l > "${config_fld}/cron/crontab"
 
-rm -rf "$config_fld/hyprland"
-mkdir -p "$config_fld/hyprland"
-cp -r $HOME/.config/hypr/* "$config_fld/hyprland"
+rmkdir copyq
+cp -f $HOME/.config/copyq/copyq.conf "${config_fld}/copyq/copyq.conf"
 
-rm -rf "$config_fld/waybar"
-mkdir -p "$config_fld/waybar"
-cp -r $HOME/.config/waybar/* "$config_fld/waybar"
+backup_cfg tmux hypr waybar onlyoffice yandex-disk flameshot pop-shell
 
-echo "($(basename $0))" "Configs saved"
+echo "$(basename $0): " "Configs saved"
 
