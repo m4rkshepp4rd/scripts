ddf60e2285bef6503d95d73971a4d0f70fc0b021
Mark Sheppard <m4rkshepp4rd@yandex.ru>
2025-11-13 23:23:28 +0500

    x symlink refactor

--- symlinks/x.sh
+++ symlinks/x.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-if [[ -z $MS_SCR ]]; then
-    echo "($(basename $0))" "Env var MS_SCR not defined"
-    exit 1
-fi
+set -e
+x-utils-check var $0 MS_SCR
+set +e
 
-rm -rf $HOME/.x
-ln -s $MS_SCR $HOME/.x
+rm -rf "$HOME/.x"
+ln -s "$MS_SCR" "$HOME/.x"
 
 if [[ " $* " == *" -l "* ]]; then
-    if [[ -z $MS_DOCS ]]; then
-        echo "($(basename $0))" "Env var MS_DOCS not defined"
-        exit 1
-    fi
+    set -e
+    x-utils-check var $0 MS_DOCS
+    set +e
     rm "$MS_SCR/_configs"
     ln -s "$MS_DOCS/configs" "$MS_SCR/_configs"
     rm "$MS_SCR/_cfg-backups"
