dec7a90ca4d0c4dec5621b282ebce4af182d76eb
Mark Sheppard <m4rkshepp4rd@yandex.ru>
2025-11-19 21:15:37 +0500

    fixed git-next

--- setup/git-next.sh
+++ setup/git-next.sh
@@ -3,13 +3,14 @@
 set -e
 x-utils-check var $0 MS_CFG
 git_cfg="$MS_CFG/git"
-x-utils-check dir "$git_cfg"
+x-utils-check dir $0 "$git_cfg"
+x-utils-check file $0 "$HOME/.git-profile"
 set +e
 
-n=$(ls -lA "$git_cfg" | grep -n "$(cat .git-profile)" | awk -F: '{print $1}')
+n=$(ls -lA "$git_cfg" | grep -n "$(cat "$HOME/.git-profile")" | awk -F: '{print $1}')
 nxt=$(ls -lA "$git_cfg" | head -n $(($n+1)) | tail -n 1 | awk '{print $NF}')
 
-[ $nxt != $(cat .git-profile) ] && x-setup-git $nxt && exit 0
+[ $nxt != $(cat "$HOME/.git-profile") ] && x-setup-git -p $nxt && exit 0
 
 first=$(ls -lA "$git_cfg" | grep -v total | head -n 1 | awk '{print $NF}')
 x-setup-git -p $first
