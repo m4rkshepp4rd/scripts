5910ad21603080ce1b9d0bfff8904a15ee6af206
Mark Sheppard <m4rkshepp4rd@yandex.ru>
2025-11-19 20:59:38 +0500

    gpg refactor

--- gpg-decrypt.sh
+++ gpg-decrypt.sh
@@ -1,44 +1,29 @@
 #!/usr/bin/env bash
 
-if [[ -z $MS_GPG_PASS ]]; then
-    echo "($(basename $0))" "Env var MS_GPG_PASS not defined"
-    exit 1
-fi
+set -e
+export source="$1"
+export target="$2"
+x-utils-check var $0 MS_GPG_PASS source target
+x-utils-check file $0 "$MS_GPG_PASS"
+x-utils-check dir $0 "$source"
+set +e
 
-
-if [[ ! -f $MS_GPG_PASS ]]; then
-    echo "($(basename $0))" "Passpharase file not found"
-    exit 1
-fi
-
-if [[ -z $1 ]]; then
-    echo "($(basename $0))" "enter source path"
-    exit 1
-fi
-
-SOURCE=$1
-if [[ -z $2 ]]; then
-    echo "($(basename $0))" "enter target path"
-    exit 1
-fi
-
-TARGET=$2
-source_base=$(basename -- $SOURCE)
+target=$2
+source_base=$(basename $source)
 mkdir -p "/tmp/$source_base/"
-mkdir -p $TARGET
-for path in ${SOURCE}/*.gpg; do
-    path_part=${path##*/}
-    base=${path_part%.*}
-    gpg --batch --passphrase-file $MS_GPG_PASS -o $TARGET/$base -d ${path}
-    # unzip ${tmp_path} -d $TARGET
+mkdir -p $target
+for path in $source/*.gpg; do
+    path_part="${path##*/}"
+    base="${path_part%.*}"
+    gpg --batch --passphrase-file "$MS_GPG_PASS" -o "$target/$base" -d "$path"
 done
 
-for path in ${SOURCE}/*.gpg.fld; do
-    path_part=${path##*/}
-    base=${path_part%.*}
+for path in $source/*.gpg.fld; do
+    path_part="${path##*/}"
+    base="${path_part%.*}"
     tmp_path="/tmp/$source_base/$base"
-    gpg --batch --passphrase-file $MS_GPG_PASS -o ${tmp_path} -d ${path}
-    unzip -o ${tmp_path} -d $TARGET
+    gpg --batch --passphrase-file "$MS_GPG_PASS" -o "$tmp_path" -d "$path"
+    unzip -o "$tmp_path" -d "$target"
 done
 
 rm -rf "/tmp/$source_base/"
--- gpg.sh
+++ gpg.sh
@@ -1,50 +1,36 @@
 #!/usr/bin/env bash
 
+set -e
+export source="$1"
+export target="$2"
+x-utils-check var $0 MS_GPG_PASS source target
+x-utils-check file $0 "$MS_GPG_PASS"
+x-utils-check dir $0 "$source"
+set +e
 
-if [[ -z $MS_GPG_PASS ]]; then
-    echo "($(basename $0))" "Env var MS_GPG_PASS not defined"
-    exit 1
-fi
 
-if [[ ! -f $MS_GPG_PASS ]]; then
-    echo "($(basename $0))" "Passpharase file not found"
-    exit 1
-fi
-
-if [[ -z $1 ]]; then
-    echo "($(basename $0))" "enter source path"
-    exit 1
-fi
-
-SOURCE=$1
-if [[ -z $2 ]]; then
-    echo "($(basename $0))" "enter target path"
-    exit 1
-fi
-
-TARGET=$2
-mkdir -p ${TARGET}
-for path in ${SOURCE}/*; do
-    base=$(basename -- "${path}")
-    echo "($(basename $0))" "encrypting ${path}"
-    if [[ -d ${path} ]]; then
+mkdir -p $target
+for path in $source/*; do
+    base=$(basename "$path")
+    echo "$(basename $0): encrypting $path"
+    
+    if [[ -d "$path" ]]; then
         cd $path/..
-        # zip -r -X /tmp/${base}.zip ${path}
-        zip -rqX /tmp/${base}.zip ${base}
-        if ! test -f "${TARGET}/${base}.sha1" || [ "$(sha1sum /tmp/${base}.zip | cut -d " " -f 1)" != "$(cat ${TARGET}/${base}.sha1)" ]; then
-            echo "($(basename $0))" "$base changed"
-            rm "${TARGET}/${base}.gpg.fld"
-            gpg --batch --passphrase-file "${MS_GPG_PASS}" -o "${TARGET}/${base}.gpg.fld" -c /tmp/${base}.zip
-            sha1sum /tmp/${base}.zip | cut -d " " -f 1 > "${TARGET}/${base}.sha1"
+        zip -rqX /tmp/$base.zip "$base"
+        if ! test -f "$target/$base.sha1" || [ "$(sha1sum /tmp/$base.zip | cut -d " " -f 1)" != "$(cat $target/$base.sha1)" ]; then
+            echo "$(basename $0): $base changed"
+            rm "$target/$base.gpg.fld"
+            gpg --batch --passphrase-file "$MS_GPG_PASS" -o "$target/$base.gpg.fld" -c "/tmp/$base.zip"
+            sha1sum /tmp/$base.zip | cut -d " " -f 1 > "$target/$base.sha1"
         fi
-        rm "/tmp/${base}.zip"
+        rm "/tmp/$base.zip"
     fi
-    if [[ -f ${path} ]]; then
-        if ! test -f "${TARGET}/${base}.sha1" || [ "$(sha1sum ${path} | cut -d " " -f 1)" != "$(cat ${TARGET}/${base}.sha1)" ]; then
-            echo "($(basename $0))" "$base changed"
-            rm "${TARGET}/${base}.gpg"
-            gpg --batch --passphrase-file "${MS_GPG_PASS}" -o "${TARGET}/${base}.gpg" -c ${path}
-            sha1sum ${path} | cut -d " " -f 1 > "${TARGET}/${base}.sha1"
+    if [[ -f "$path" ]]; then
+        if ! test -f "$target/$base.sha1" || [ "$(sha1sum $path | cut -d " " -f 1)" != "$(cat $target/$base.sha1)" ]; then
+            echo "$(basename $0): $base changed"
+            rm "$target/$base.gpg"
+            gpg --batch --passphrase-file "$MS_GPG_PASS" -o "$target/$base.gpg" -c "$path"
+            sha1sum $path | cut -d " " -f 1 > "$target/$base.sha1"
         fi
     fi
 done
