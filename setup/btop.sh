f854247da77b7e30add2f3bbb0530ab986c0f89b
Mark Sheppard <m4rkshepp4rd@yandex.ru>
2025-11-20 12:54:59 +0500

    btop

--- setup/btop.sh
+++ setup/btop.sh
@@ -8,4 +8,6 @@ export config_fld=$(x-utils-cfg-get-path $@)
 x-utils-check var $0 config_fld
 x-utils-cmd-install $CMD
 x-utils-cfg-install $config_fld $DEST "$@"
+mkdir -p "$DEST/themes"
+ls -s "$DEST/themes/current.theme" "$HOME/.config/omarchy/current/theme/btop.theme" &> /dev/null
 set +e
