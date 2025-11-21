ed842cd4df56252b4d36b33afce0c18618f1a3dc
Mark Sheppard <m4rkshepp4rd@yandex.ru>
2025-11-21 17:58:40 +0500

    smaller gaps in launch list

--- index.sh
+++ index.sh
@@ -24,7 +24,7 @@ while read file; do
     name=$(grep -E "^Name=" "$file" | head -1 | cut -d= -f2)
     no_display=$(grep -E "^NoDisplay=" "$file" | head -1 | cut -d= -f2)
     if [[ ! -z "$name" && ! " ${names[*]} " =~ " $name " ]]; then
-        [[ "$no_display" != "true" ]] && echo -n "$name" &&  printf '%*s' 100 && echo "| $file" && names+=("$name")
+        [[ "$no_display" != "true" ]] && echo -n "$name" &&  printf '%*s' $((70-${#name})) && echo "| $file" && names+=("$name")
     fi
 done < <(find /usr/share/applications /usr/local/share/applications "$HOME/.local/share/applications" -name "*.desktop" 2>/dev/null) | sort > "/tmp/.desktop-apps"
 
